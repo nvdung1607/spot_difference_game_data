d0516093555e050664f036defada3a336bdb10be
nvdung1607 <nvdung1607@gmail.com>
2023-03-29 16:25:51 +0700

    fix bug task 2

--- task2.py
+++ task2.py
@@ -13,7 +13,6 @@ img2 = cv2.resize(img2, (600, 600))
 #Grayscale
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
 #find the difference
 diff = cv2.absdiff(gray1, gray2)
 # cv2.imshow('diff(img1, img2)', diff)
@@ -24,8 +23,8 @@ thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 #Dilation
 kernel = np.ones((5,5), np.uint8)
-dilate = np.ones(thresh, kernel, iterations=2)
-# cv2.imshow('Dilation', dilate)
+dilate = cv2.dilate(thresh, kernel, iterations=2)
+cv2.imshow('Dilation', dilate)
 
 #Find contours
 contours = cv2.findContours(dilate.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -40,8 +39,8 @@ for contour in contours:
         
 #show final images
 x = np.zeros((600, 10, 3), np.uint8)
-result = np.hstack((img1, img2))
-cv2.imshow("diffences", kernel)
+result = np.hstack((img1, x, img2))
+cv2.imshow("diffences", result)
 
 # cv2.imshow('Original Img', img1)
 # cv2.imshow('Edited Img', img2)
